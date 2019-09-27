94911e76a4bf2e69273da84fc920eb1f82fe35fa
ihatepointers <51532583+ihatepointers@users.noreply.github.com>
2019-09-27 13:39:39 +0300

    Update Service1.cs

--- Voice Picking/WCF Service/library/Service1.cs
+++ Voice Picking/WCF Service/library/Service1.cs
@@ -29,44 +29,6 @@ namespace GettingStartedLib
         static List<(int, string, string, int, string)> tmpItems = new List<(int, string, string, int, string)>();
 
 
-        public double Add(double n1, double n2)
-        {
-            double result = n1 + n2;
-            Console.WriteLine("Received Add({0},{1})", n1, n2);
-            Console.WriteLine("Return: {0}", result);
-            return result;
-        }
-
-        public double Subtract(double n1, double n2)
-        {
-            double result = n1 - n2;
-            Console.WriteLine("Received Subtract({0},{1})", n1, n2);
-            Console.WriteLine("Return: {0}", result);
-            return result;
-        }
-
-        public double Multiply(double n1, double n2)
-        {
-            double result = n1 * n2;
-            Console.WriteLine("Received Multiply({0},{1})", n1, n2);
-            Console.WriteLine("Return: {0}", result);
-            return result;
-        }
-
-        public double Divide(double n1, double n2)
-        {
-            double result = n1 / n2;
-            Console.WriteLine("Received Divide({0},{1})", n1, n2);
-            Console.WriteLine("Return: {0}", result);
-            return result;
-        }
-
-        public String GetText()
-        {
-            return "Hello";
-        }
-
-
         public String MirrorText(string text)
         {
             return text;
