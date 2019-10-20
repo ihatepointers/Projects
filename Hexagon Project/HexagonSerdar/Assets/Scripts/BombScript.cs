6485ea78713b51c6c0a25572b7b92df9a1002300
ihatepointers <51532583+ihatepointers@users.noreply.github.com>
2019-10-20 21:02:01 +0300

    Hexagon Project

---
+++ Hexagon Project/HexagonSerdar/Assets/Scripts/BombScript.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class BombScript : MonoBehaviour
+{
+
+    public TextMeshProUGUI bombText;
+    private int remainingRounds = 8;
+    AudioSource audioSource;
+    public AudioClip hissClip;
+    public AudioClip explosionClip;
+
+    void Start()
+    {
+        bombText.text = remainingRounds.ToString();
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public int tick()
+    {
+        //progress the timer and update the text
+        remainingRounds--;
+        bombText.text = remainingRounds.ToString();
+        return remainingRounds;
+    }
+
+    public void setBomb()
+    {
+        //Start the bomb with necessary objects
+        audioSource = GetComponent<AudioSource>();
+        bombText = this.transform.GetChild(1).transform.GetChild(0).transform.GetChild(0).GetComponent<TextMeshProUGUI>();
+        this.remainingRounds = 8;
+        audioSource.PlayOneShot(hissClip, 1.0F);
+        bombText.text = remainingRounds.ToString();
+
+    }
+
+    public void explode()
+    {
+        audioSource.PlayOneShot(explosionClip, 1.0F);
+
+    }
+}
---
+++ Hexagon Project/HexagonSerdar/Assets/Scripts/HexagonScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HexagonScript : MonoBehaviour
+{
+
+    public int positionX;
+    public int positionY;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void setPositions(int x, int y)
+    {
+        positionX = x;
+        positionY = y;
+    }
+
+    public int getPositionX()
+    {
+        return positionX;
+    }
+
+    public int getPositionY()
+    {
+        return positionY;
+    }
+
+    
+
+}
